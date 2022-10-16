776eea9c26d782983f25a9636db4f51515349af9
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-16 14:14:45 +0530

    Create whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,40 @@
+# i = 1
+# while True:
+#     if i + 1 > 3:
+#         i = i + 1
+#         continue
+#     print(i, end = " ")
+#     if i == 45:
+#         i = i + 1
+#         break
+#     i = i + 1
+
+# Program to check if a number is prime or not
+
+#num = int(input("Enter a number: "))
+#flag = False
+
+#if num > 1:
+#    for i in range(2, num):
+#        if (num % i) == 0:
+#            flag = True
+#            break
+
+#if flag:
+#    print(num, "is not a prime number")
+#else:
+#    print(num, "is a prime number")
+
+
+
+
+
+# i = 1
+# while i<=5:
+#     print("hello", end = " ")
+#     j = 1
+#     while j<=4:
+#         print("world", end = " ")
+#         j = j + 1
+#     i = i + 1
+#     print()
