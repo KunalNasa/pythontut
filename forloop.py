c4712e64eb6bc6a8e3732269b3bb810a98170a3b
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-16 14:14:01 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,35 @@
+# x = ["kannu", "mannu", 20, 1.25]
+# for i in x:
+#     print(i)
+# list = ["tinku", "pinku", "minku", "chinku"]
+#
+# for i in list:
+#    print(i, "is a good boy")
+
+
+# list = [1, "guddu", 2, 5, 7, 8, "monu", "sonu" ]
+#
+# for i in list:
+#    if str(i).isalpha( ):
+#        print(i)
+
+#list = ["banana", "apple", "mango", "grapes", "cherry", "lichi", "guava"]
+#for i in list:
+#    if i == "grapes":
+#        continue
+#    print(i)
+
+#for i in range (2, 10, 3):
+#    print(i)
+#fruits = {"banana":"carrot" , "apple":"raddish" , "mango":"potato", "grapes":"ladyfinger"}
+#for x,y in fruits.items():
+#     print(x,y)
+
+# print("enter an integer")
+# int(input())
+
+
+
+# for i in range(0,100):
+#     if i%5 != 0:
+#         print(i)
