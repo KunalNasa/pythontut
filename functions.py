144ce2c38f66b4532b0e9b69ab00326aafc0255f
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-16 14:20:55 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,31 @@
+"""
+i = 0
+while True:
+    print(f"the value of i is : {i}") #fstring
+    i = i + 1
+    if i > 10:
+        print("breaking the loop")
+        break;
+"""
+
+#def function1(a, b):
+#    print("you are in function", a + b)
+#(function1(4,5))
+
+#def function2(a, b):
+#  """this function calculates the average of two numbers"""
+#  average = (a + b)/2
+#  #print(average)
+#  return average
+
+#x = function2(3,5)
+#print(x)
+#print(function2.__doc__)
+
+#def multiplication(a, b):
+#  product = a*b
+#  #print(product)
+#  return product
+
+#k = multiplication(7,9)
+#print(k)
