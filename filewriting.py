b71a5361e019f4a6128cdb0946b395aed7efe121
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-16 14:23:39 +0530

    Create filewriting.py

---
+++ filewriting.py
@@ -0,0 +1,21 @@
+# f = open("kannu.txt", "rt")
+# #content = f.read()
+# for line in f:
+#     print(line, end="")
+# # print(content)
+# # content = f.read(5)
+# # print(content)
+# f.close()
+
+
+# f = open("kannu.txt")
+# print(f.readlines())
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+
+k = open("kannu.txt")
+
+content = k.read()
+print(content)
+k.close()
