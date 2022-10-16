893afd9750b4e97365e3890846fce226b37fcd09
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-16 14:19:47 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,15 @@
+"""
+Arithmetic operators
+Assignment operators
+Comparison operators
+
+Identity operators
+Membership operators
+Bitwise operators
+"""
+#Logical operators
+a = False
+b = False
+
+print(a is not b)
+
