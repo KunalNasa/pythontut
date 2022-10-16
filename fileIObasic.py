6348e427275d476b0eaa7df22f5247c07e2e6e69
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-16 14:22:59 +0530

    Create fileIObasic.py

---
+++ fileIObasic.py
@@ -0,0 +1,14 @@
+# note:
+# for commenting multiple lines like this
+#     select all the lines and press
+# command + /
+
+"""
+"r" - open file for reading - default
+"w" - open file for writing
+"x" - creates a file if not exists
+"a" - add more content to a file
+"t" - text mode - default
+"b" - binary mode
+"+" - read and write
+"""
