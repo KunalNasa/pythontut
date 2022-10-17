e0ddc32b38eae2f56b1651c57b4ab51842baa379
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-17 22:18:56 +0530

    Create astronautstars.py

---
+++ astronautstars.py
@@ -0,0 +1,14 @@
+# n = int(input("enter number of rows\n"))
+# k = int(input("enter 0 or 1\n"))
+# if k==True:
+#    x=1
+#    while x<=n:
+#     print("*"*x)
+#     x = x + 1
+# else:
+#     x=n
+#     while x>0:
+#         print("*"*x)
+#         x = x - 1
+#
+#
