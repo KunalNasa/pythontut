d4ea3836bdcc7e2c9d11a0896847aa169499ac88
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-21 16:24:30 +0530

    Create moreonfiles.py

---
+++ moreonfiles.py
@@ -0,0 +1,12 @@
+# f = open("kannu2.txt")
+# f.seek(0)
+# print(f.tell())
+# print(f.readline())
+# print(f.tell())
+# print(f.readline())
+# print(f.tell())
+# print(f.readline())
+# print(f.tell())
+#
+#
+# f.close()
