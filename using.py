1d27b96aa090d8d3faaf6e8c9460b05e2f741f1b
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-21 16:31:11 +0530

    Create using.py

---
+++ using.py
@@ -0,0 +1,19 @@
+# import random
+# random_nummber = random.randint(0,1)
+# # print(random_nummber)
+# rand =  random.random()
+# # print(rand*100)
+# lst = ["kannu", "mannu", "channu", "tannu", "sannu"]
+# choice = random.choice(lst)
+# print(choice)
+
+
+
+# import pyautogui
+# import time
+# time.sleep(5)
+# count = 0
+# while count<=100:
+#     pyautogui.typewrite("hello somia"+ str(count))
+#     pyautogui.press("enter")
+#     count = count + 1
