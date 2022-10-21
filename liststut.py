659f00ca9017951fbf80b2c027cb7d2980eed561
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-21 17:20:39 +0530

    Create liststut.py

---
+++ liststut.py
@@ -0,0 +1,11 @@
+colleges = ["DTU", "NITH", "NSUT", "USICT"]
+colleges[1] = "NITKKR"
+x = colleges.copy()
+colleges.insert(3, "IIT")
+y = colleges.count("IIT")
+print(y)
+print(colleges)
+print(x)
+#tup = (2, 3, 4)
+#print(tup)
+
