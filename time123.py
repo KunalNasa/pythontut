bea32f1704c4e10f52e2f2e0f1c0c1cf746d2a56
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-25 12:03:26 +0530

    Create time123.py

---
+++ time123.py
@@ -0,0 +1,31 @@
+# import time
+#
+#
+# initial2 = time.time()
+# for i in range(45):
+#     print("this is kannu")
+#     time.sleep(5)
+# print("for loop ran in", time.time() - initial2, "seconds")
+#
+# initial = time.time()
+# k = 0
+# while k<45:
+#     print("this is kannu")
+#     k = k + 1
+# print("while loop "
+#       "ran in", time.time() - initial, "seconds")
+
+
+#code to know time and date and year
+# localtime = time.asctime(time.localtime(time.time()))
+# print(localtime)
+
+
+# import time
+
+# Convert the current time in seconds
+# since the epoch to a
+# time.struct_time object in Local time
+# obj = time.localtime()
+
+# print(obj)
