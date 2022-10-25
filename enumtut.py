90665803712ae1691a59521c405b843fdaf48f9d
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-10-25 12:03:57 +0530

    Create enumtut.py

---
+++ enumtut.py
@@ -0,0 +1,15 @@
+# ls1 = ["kaddu", "gobi", "bhindi", "tamatar", "bengan"]
+# i = 1
+# for item in ls1:
+#    if i%2 !=0:
+#        print(f"pleas buy {item}")
+#    i = i +1
+
+# for index, item in enumerate(ls1):  #index starts from 0
+#     if index%2 ==0:
+#         print(f"please buy {item}")
+
+
+# for index, item in enumerate(ls1, start=1):  # index starts from 1
+#     if index % 2 != 0:
+#         print(f"please buy {item}")
