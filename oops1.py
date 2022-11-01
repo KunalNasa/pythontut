3aa0ab3eec8ddcd9e9fe84c31c250ffb289f3a35
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-11-02 00:09:57 +0530

    Create oops1.py

---
+++ oops1.py
@@ -0,0 +1,16 @@
+# class - template
+# object - instance of the Class
+
+# DRY - Do Not Repeat Yourself
+
+class student:
+    pass
+
+kannu = student()
+mannu = student()
+
+kannu.name = "kannu"
+kannu.std = 12
+kannu.section = 1
+mannu.subjects = ["hindi", "english", "maths"]
+print(mannu.subjects)
