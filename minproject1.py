b826fc2dd8adbb8b023eb02a865869c1fddf5290
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-12-05 19:47:46 +0530

    Add files via upload

---
+++ minproject1.py
@@ -0,0 +1,25 @@
+# list_of_books = ["Wings of Fire", "Rich Dad Poor Dad", "Atomic Habits", "Ikigai", "Concepts of Physics", "j"]
+# print(list_of_books)
+#
+#
+# class Library():
+#     # def printbooks(self):
+#     #     print(list_of_books)
+#
+#     def lendbook(self):
+#         for i in list_of_books:
+#             print(i)
+#
+#     x = input("enter the name of book")
+#     y = input("enter your name")
+#     if x in list_of_books:
+#         y = list_of_books.remove(x)
+#     print(y)
+#
+# j = Library()
+# k = Library()
+# # j.printbooks()
+# j.lendbook()
+
+
+
