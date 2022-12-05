27a07c284489d143a75d2c92243255a5e2001130
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-12-05 19:35:45 +0530

    Add files via upload

---
+++ oops11.py
@@ -0,0 +1,8 @@
+#polymorphism - alag alag tarah ke roop dharan kar lena. e.g 👇
+print(5 + 6)
+print("5"+"6")
+
+# Abstraction - Abstraction is hiding the details and implementation of the code.
+# Encapsulation - Encapsulation is hiding the data and controlling the visibility of the code.
+# inheritance
+# polymorphism
