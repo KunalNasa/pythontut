f160c9e860c4caa257529a6ac172f7f1bf2f2d38
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-12-05 20:02:11 +0530

    Add files via upload

---
+++ forelsse.py
@@ -0,0 +1,12 @@
+masale = ["laal mirch", "kaali mirch", "namak", "haldi"]
+
+for i in masale:
+    if i == "namakeen":
+        print(i)
+        break
+else:
+    print("YOUR ITEM IS NOT MASALA")
+
+# import inspect
+# import math
+# print(inspect.getmembers(math))
