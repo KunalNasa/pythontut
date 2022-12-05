15462d778af223a418ef6a03d1fd6b0f4c65c4e6
KunalMLAI <111643119+KunalMLAI@users.noreply.github.com>
2022-12-05 20:04:03 +0530

    Add files via upload

---
+++ reqtut.py
@@ -0,0 +1,17 @@
+import requests
+# r = requests.get("https://site.financialmodelingprep.com/financial-summary/AAPL")
+# print(r.text)
+# print(r.status_code)
+
+# url = "www.kunalnasa.com"
+# data = {
+#     "p":4,
+#     "p2":8
+# }
+#
+# r2 = requests.post(url= url, data=data)
+
+
+# x = requests.get('https://w3schools.com/python/demopage.htm')
+#
+# print(x.text)
